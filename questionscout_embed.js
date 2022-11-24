426c744b77d35d11db4808f32a8b19624cbea2b6
Stockton <stockton.fisher@gmail.com>
2022-11-23 22:02:13 -0700

    added questionscout form with dynamic values

---
+++ questionscout_embed.js
@@ -0,0 +1,12 @@
+<script id="qs-embed-63613e4a203d8120aa1e6421">
+    (function (p) {
+        var s = window.document.createElement('script');
+    s.src = p.a;
+    s.setAttribute('data-form-id', p.b);
+    s.setAttribute('data-url-params', p.c);
+    s.setAttribute('data-runner-id', p.d);
+    s.setAttribute('data-css', p.e);
+    s.setAttribute('data-scale', true);
+    window.document.head.appendChild(s);
+    })({"a": "https://form.questionscout.com/qs-form-script.min.js", "b": "63613868efdc2d40275e925a", "c": "[{\"key\":\"email\",\"value\":\"" + _thrive.customer.email + "\"},{\"key\":\"first_name\",\"value\":\"" + _thrive.customer.firstname + "\"}]", "d": "qs-embed-63613e4a203d8120aa1e6421", "e": "" })
+</script>
