3be9a8be6747278d5de7a12dd5fdc22efd633e7a
Stockton <stockton.fisher@gmail.com>
2023-01-17 20:32:43 -0800

    added user data to all product pages

--- all_product_pages.js
+++ all_product_pages.js
@@ -12,6 +12,41 @@ function makeid(length) {
   return result;
 }
 
+function standardize(input) {
+  if (input == null || undefined) return input;
+  else return input.trim().toLowerCase();
+}
+
+var products = [];
+var user_data = {};
+var address = {};
+user_data.email = standardize(_thrive.customer.email); // required https://support.google.com/google-ads/answer/10172785#zippy=%2Cenable-enhanced-conversions-in-google-tag-manager%2Cidentify-and-define-your-enhanced-conversions-variables%2Cpre-hashed-data-is-being-provided-in-your-enhanced-conversions%2Cvariables-werent-successfully-implemented-for-enhanced-conversions%2Ccode-copying-errors%2Cfind-enhanced-conversions-variables-on-your-conversion-page%2Cidentify-enhanced-conversions-css-selectors-and-input-into-google-tag-manager:~:text=Address%20(first%20name%2C%20last%20name%2C%20postal%20code%2C%20country%20are%20required%20if%20you%20choose%20to%20use%20this%20data)
+user_data.phone_number = standardize(_thrive.customer.contactno);
+address.first_name = standardize(_thrive.customer.firstname); // required
+address.last_name = standardize(_thrive.customer.lastname); // required
+address.street = standardize(_thrive.customer.address.line1);
+address.city = standardize(_thrive.customer.address.city);
+address.region = standardize(_thrive.customer.address.state);
+address.postal_code = standardize(_thrive.customer.address.zip); // required
+address.country = standardize(_thrive.customer.address.country); // required
+user_data.address = address;
+
+dataLayer = window.dataLayer || [];
+dataLayer.push({
+  event: "user_data",
+  user_data: user_data,
+  meta_details: {
+    customer_id: _thrive.customer.id,
+    custom_fields: _thrive.customer.custom_fields,
+    client_meta_fbc: _thrive.customer.client_meta_fbc,
+    client_meta_fbp: _thrive.customer.client_meta_fbp,
+    client_user_agent: _thrive.customer.client_user_agent,
+    ip_address: _thrive.customer.ip_address,
+    optin: _thrive.customer.optin,
+    terms_and_conditions: _thrive.customer.tandc
+  }
+});
+
 if (_thrive.offer) {
   var dataLayer = window.dataLayer || [];
   dataLayer.push({
--- customer_labs_email_listener.js
+++ customer_labs_email_listener.js
@@ -1,38 +1,40 @@
-var fields = document.querySelectorAll('input[name="email"], input[data-type*="email"], input[type*="email"], input[placeholder*="email"], input[aria-label*="email"], input[data-name*="Email"]');
-fields.forEach(function (field) {
-    field.addEventListener('change', function () {
-        var em = field.value || $('input:visible[name="email"]').val();
-        console.log('field changed', em);
-        var properties = {
-            "customProperties": {
-                "user_traits": {
-                    "t": "Object",
-                    "v": {
-                        "email": {
-                            "t": "string",
-                            "v": em || $('input:visible[name="email"]').val()
-                        },
-                        "first_name": {
-                            "t": "string",
-                            "v": $('input:visible[name="first_name"], input:visible[type="first_name"], input:visible[aria-label="first_name"], input:visible[name="Name"], input:visible[data-name="Name"], input:visible[aria-label*="Name"]').val()
-                        },
-                        "last_name": {
-                            "t": "string",
-                            "v": $('input:visible[name="last_name"], input:visible[type="last_name"], input:visible[aria-label="last_name"]').val()
-                        },
-                        "phone": {
-                            "t": "string",
-                            "v": $('input:visible[name="phone"], input:visible[type="phone"], input:visible[aria-label="phone"], input:visible[type="tel"]').val()
+window.onload = function () {
+    var fields = document.querySelectorAll('input[name="email"], input[data-type*="email"], input[type*="email"], input[placeholder*="email"], input[aria-label*="email"], input[data-name*="Email"]');
+    fields.forEach(function (field) {
+        field.addEventListener('change', function () {
+            var em = field.value || $('input:visible[name="email"]').val();
+            console.log('field changed', em);
+            var properties = {
+                "customProperties": {
+                    "user_traits": {
+                        "t": "Object",
+                        "v": {
+                            "email": {
+                                "t": "string",
+                                "v": em || $('input:visible[name="email"]').val()
+                            },
+                            "first_name": {
+                                "t": "string",
+                                "v": $('input:visible[name="first_name"], input:visible[type="first_name"], input:visible[aria-label="first_name"], input:visible[name="Name"], input:visible[data-name="Name"], input:visible[aria-label*="Name"]').val()
+                            },
+                            "last_name": {
+                                "t": "string",
+                                "v": $('input:visible[name="last_name"], input:visible[type="last_name"], input:visible[aria-label="last_name"]').val()
+                            },
+                            "phone": {
+                                "t": "string",
+                                "v": $('input:visible[name="phone"], input:visible[type="phone"], input:visible[aria-label="phone"], input:visible[type="tel"]').val()
+                            }
                         }
+                    },
+                    "identify_by_email": {
+                        "t": "string",
+                        "v": em,
+                        "ib": true
                     }
-                },
-                "identify_by_email": {
-                    "t": "string",
-                    "v": em,
-                    "ib": true
                 }
-            }
-        };
-        _cl.identify(properties);
+            };
+            _cl.identify(properties);
+        });
     });
-});
+}
